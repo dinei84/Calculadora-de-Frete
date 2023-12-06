217c3e973ee0c13ce9d42d2f99994afb64312362
devdinei <devdinei@gmail.com>
2023-12-06 00:02:52 -0300

    mais upgrades

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
-function calcular() {
-  console.log("Função calcular() chamada");
-  // var fe = parseFloat(document.getElementById('fe').value) || 0;
-  // var pd = parseFloat(document.getElementById('pd').value) || 0;
-  // var peso = parseFloat(document.getElementById('peso').value) || 0;
+function calcular() { 
+  var fe = parseFloat(document.getElementById('fe').value) || 0;
+  var pd = parseFloat(document.getElementById('pd').value) || 0;
+  var peso = parseFloat(document.getElementById('peso').value) || 0;
 
-  // var pesoArredondado = Math.round(peso);
-  // var valorPedagioPorQuilo = pd / pesoArredondado;
-  // var resultado = fe - valorPedagioPorQuilo;
+  var pesoArredondado = Math.round(peso);
+  var valorPedagioPorQuilo = Number (pd / pesoArredondado);
+  var resultado = Number (fe - valorPedagioPorQuilo);
 
-  // document.getElementById('res').value = resultado.toFixed(2);
+  document.getElementById('res').value = resultado.toFixed(2);
 }
